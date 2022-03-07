3b43cf7fcc306b2b703f83f5bf6849fb09c2a75c
Santiago Gonzalez <santiagogonzalez131@gmail.com>
2022-03-07 18:09:12 -0300

    Aplicando sweetalert

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
   let saludoStorage = localStorage.getItem(welcome);
   let nameStorage = localStorage.getItem('nombreUsuario');
   let lastNameStorage = localStorage.getItem('apellidoUsuario');
-  let anioNacimientoStorage = localStorage.getItem('anioNacimientoUsuario');
   let emailStorage = localStorage.getItem('emailUsuario');
-  let cuantosStorage = localStorage.getItem('cuantosUsuario');
-  let paisesStorage = localStorage.getItem('paisesUsuario');
+  let passwordStorage = localStorage.getItem('passwordUsuario');
+  let anioNacimientoStorage = localStorage.getItem('anioNacimientoUsuario');
   const contFormulario = document.getElementById("contFormulario");
+  const anioNacimiento = document.getElementById('anioNacimiento');
 
   const YEAR = 2022;
   let edad = (YEAR - anioNacimiento);
@@ -16,23 +16,71 @@
 
   //Variables DOM
   const greeting = document.getElementById("greeting");
+  
+  const reset = document.getElementById("reset");
+  const borrar = document.getElementById("borrar");
+
   const form = document.getElementById("form");
   const contForm = document.getElementById("contForm");
-
-    form.addEventListener("submit", (e) =>{
-      e.preventDefault();
-      localStorage.setItem('nombreUsuario', form.children[0].value);
-      localStorage.setItem('apellidoUsuario', form.children[1].value);
-      localStorage.setItem('anioNacimientoUsuario', form.children[2].value);
-      localStorage.setItem('emailUsuario', form.children[3].value);
-      localStorage.setItem('cuantosUsuario', form.children[4].value);
-      localStorage.setItem('paisesUsuario', form.children[5].value);
-      nameStorage = form.children[0].value;
-      lastNameStorage = form.children[1].value;
-      checkForm();
-    })
-
+  const modal_container = document.getElementById("modal_container");
+  
+  const close = document.getElementById('close');
   const send = document.getElementById("send");
+  const open = document.getElementById('open');
+
+  const searchform = document.getElementById('search-form');
+  const searchbox = document.getElementById('searchbox');
+
+  const otra = document.getElementById('otra');
+
+  const nombre = document.getElementById('nombre');
+  const apellido = document.getElementById('apellido');
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
+
+  const nombreUno = document.getElementById('nombreUno');
+  const apellidoUno = document.getElementById('apellidoUno');
+  const emailUno = document.getElementById('emailUno');
+  const passwordUno = document.getElementById('passwordUno');
+  const anioNacimientoUno = document.getElementById('anioNacimientoUno');
+
+  form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    localStorage.setItem('nombreUsuario', form.children[0].value);
+    localStorage.setItem('apellidoUsuario', form.children[1].value);
+    localStorage.setItem('emailUsuario', form.children[2].value);
+    localStorage.setItem('passwordUsuario', form.children[3].value);
+    localStorage.setItem('anioNacimientoUsuario', form.children[4].value);
+    nameStorage = form.children[0].value;
+    lastNameStorage = form.children[1].value;
+
+    if (nombre && apellido && email && password && anioNacimiento) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Algun campo se encuentra vacio.',
+      }) 
+      }else{
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 1500,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+        })
+        
+        Toast.fire({
+          icon: 'success',
+          title: 'Registrado con Éxito'
+        })
+      }
+
+      checkForm();
+  })
 
    send.onclick = () => {
     modal_container.classList.remove("show");
@@ -40,29 +88,50 @@
 
   const checkForm = () => {
     if (nameStorage && nameStorage !== 'null'){
-      console.log("La informacion ya existe");
       const element = document.createElement("div");
       contFormulario.remove();
       element.innerHTML = `<p class="Color">Hola ${nameStorage} ${lastNameStorage} te estabamos esperando</p>`;
       greeting.prepend(element);
+      eliminarDatos();
+      borrar.remove();
      }else{
-      console.log("La informacion no exixte");
+      searchbox.addEventListener("click",()=>{
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Debes loguearte primero',
+        })
+    })
+    }
+  }
+
+  function eliminarDatos () {
+    const otherElement = document.createElement("div");
+    otherElement.innerHTML = `
+    <div class="logginn">
+    <button class="login" id="resetear">Resetear Datos</button>
+    </div>`;
+    reset.prepend(otherElement);
+    otherElement.onclick = () => {
+      localStorage.removeItem('nombreUsuario');
+      localStorage.removeItem('apellidoUsuario');
+      localStorage.removeItem('anioNacimientoUsuario');
+      localStorage.removeItem('emailUsuario');
+      localStorage.removeItem('passwordUsuario');
+      location.reload();
     }
   }
 
   checkForm();
 
-  const open = document.getElementById('open');
-  const modal_container = document.getElementById("modal_container");
-  const close = document.getElementById('close');
-
-  open.addEventListener('click', () =>{
+   open.addEventListener('click', () =>{
     modal_container.classList.add("show");
   })
 
   close.addEventListener('click', () =>{
     modal_container.classList.remove("show");
-  })
+  }) 
+
 
   const card = document.getElementById('card')
   const city = document.getElementById('city');
@@ -83,7 +152,7 @@
     } 
     tempImg.src = src;
   }
-  
+
   async function search(query) {
     try {
       const response = await fetch(`${api.url}?q=${query}&appid=${api.key}&lang=es`);
@@ -95,8 +164,11 @@
       weather.innerHTML = data.weather[0].description;
       Images(data);
     } catch (err) {
-      console.log(err);
-      alert('Ingresa otro dato');
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Ingresa otro lugar',
+      })
     }
   }
 
@@ -108,15 +180,6 @@
     event.preventDefault();
     search(searchbox.value);
   }
-  
-  const searchform = document.getElementById('search-form');
-  const searchbox = document.getElementById('searchbox');
+
   searchform.addEventListener('submit', onSubmit, true);
 
-
-  searchbox.addEventListener("click",()=>{
-      console.log("Se hizo click en buscar el país");
-  })
-
-
-// localStorage.clear(); // Va a borrar todo en el localStorage
